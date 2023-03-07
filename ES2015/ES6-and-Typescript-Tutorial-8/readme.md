4ac97c7fc3088c17ef75aa8870860e5cf0e0515c
Srinivasa Rao Gurram <srinivasarao.gurram@gmail.com>
2023-03-07 10:30:07 -0600

    added Tutorial 8

---
+++ ES2015/ES6-and-Typescript-Tutorial-8/readme.md
@@ -0,0 +1,2 @@
+![About Constants](./docs/2023-03-07-10-11-43.png)
+![Abut Constants With Code](./docs/2023-03-07-10-22-15.png)
---
+++ ES2015/ES6-and-Typescript-Tutorial-8/tutorial8.js
@@ -0,0 +1,14 @@
+console.log("HelloWorld!");
+let num1;
+// constants should initiased during decaration
+// cannot modify const value
+const num2 = 10;
+// In below object obj1 can not be assigned with {};
+// I can change attribute value
+
+const obj1 = {
+name:"Joatmon"
+};
+console.log(obj1.name)
+obj1.name = "Chandler";
+console.log(obj1.name);
